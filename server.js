6b55c1f2b48d9899468b1878f6186b455f4a3963
Akhil001247 <2006akhilraju2@gmail.com>
2025-07-16 21:07:48 +0530

    modified files are added

--- server.js
+++ server.js
@@ -81,5 +81,5 @@ app.patch('/post/:id', async (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port is ${PORT}`);
 });
